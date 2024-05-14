37ac09a4b8c06fac0dc8c47e6be7260741997158
camtrik <camtrikcsky@gmail.com>
2024-05-14 21:04:21 +0900

    error processing

---
+++ pkg/errcode/common_code.go
@@ -0,0 +1,13 @@
+package errcode 
+
+var (
+	Success                   = NewError(0, "Success")
+	ServerError               = NewError(10000000, "Internal Server Error")
+	InvalidParams             = NewError(10000001, "Invalid Parameters")
+	NotFound                  = NewError(10000002, "Not Found")
+	UnauthorizedAuthNotExist  = NewError(10000003, "Authentication Failed: AppKey and AppSecret Not Found")
+	UnauthorizedTokenError    = NewError(10000004, "Authentication Failed: Invalid Token")
+	UnauthorizedTokenTimeout  = NewError(10000005, "Authentication Failed: Token Timeout")
+	UnauthorizedTokenGenerate = NewError(10000006, "Authentication Failed: Token Generation Failed")
+	TooManyRequests           = NewError(10000007, "Too Many Requests")
+)
---
+++ pkg/errcode/errcode.go
@@ -0,0 +1,75 @@
+package errcode
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// private field
+type Error struct {
+	code    int      `json:"code"`
+	msg     string   `json:"msg"`
+	details []string `json:"details"`
+}
+
+var codes = map[int]string{}
+
+func NewError(code int, msg string) *Error {
+	if _, ok := codes[code]; ok {
+		panic(fmt.Sprintf("Error code %d already exists", code))
+	}
+	codes[code] = msg
+	return &Error{code: code, msg: msg}
+}
+
+func (e *Error) Code() int {
+	return e.code
+}
+
+func (e *Error) Msg() string {
+	return e.msg
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("error code: %d, error message: %s", e.Code(), e.Msg())
+}
+
+// interface{} is a type that represents any value in Go.
+func (e *Error) Msgf(args []interface{}) string {
+	// args ... : all the parameters passed in
+	return fmt.Sprintf(e.msg, args...)
+}
+
+func (e *Error) Details() []string {
+	return e.details
+}
+
+func (e *Error) WithDetails(details ...string) *Error {
+	newError := *e
+	newError.details = []string{}
+	newError.details = append(newError.details, details...)
+	return &newError
+}
+
+func (e *Error) StatusCode() int {
+	switch e.Code() {
+	case Success.Code():
+		return http.StatusOK
+	case ServerError.Code():
+		return http.StatusInternalServerError
+	case InvalidParams.Code():
+		return http.StatusBadRequest
+	case UnauthorizedAuthNotExist.Code():
+		fallthrough
+	case UnauthorizedTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenTimeout.Code():
+		fallthrough
+	case UnauthorizedTokenGenerate.Code():
+		return http.StatusUnauthorized
+	case TooManyRequests.Code():
+		return http.StatusTooManyRequests
+	}
+
+	return http.StatusInternalServerError
+}
