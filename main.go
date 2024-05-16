440be17e60a89e86f1471b38974658ff92c453a5
camtrik <camtrikcsky@gmail.com>
2024-05-16 18:12:39 +0900

    set global config by viper

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/camtrik/gin-blog/global"
 	"github.com/camtrik/gin-blog/internal/routers"
+	"github.com/camtrik/gin-blog/pkg/setting"
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
+
 func main() {
+	// log.Printf("global.ServerSetting: %+v\n", global.ServerSetting)
+	// log.Printf("global.AppSetting: %+v\n", global.AppSetting)
+	// log.Printf("global.DatabaseSetting: %+v\n", global.DatabaseSetting)
+
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 
 	s := &http.Server{
@@ -18,4 +34,26 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
